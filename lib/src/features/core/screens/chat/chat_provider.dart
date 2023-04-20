e37087392a131f7f30c81dcda6898627fad8c658
officialabd <officialabdib@gmail.com>
2023-04-21 02:39:55 +0300

    created chatting provider

---
+++ lib/src/features/core/screens/chat/chat_provider.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:noorbot_app/src/constants/firestore_constants.dart';
+import 'package:noorbot_app/src/features/core/models/chat/chat.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ChatProvider {
+  final SharedPreferences prefs;
+  final FirebaseFirestore firebaseFirestore;
+  final FirebaseStorage firebaseStorage;
+
+  ChatProvider(
+      {required this.firebaseFirestore,
+      required this.prefs,
+      required this.firebaseStorage});
+
+  String? getPref(String key) {
+    return prefs.getString(key);
+  }
+
+  Future<void> updateDataFirestore(String collectionPath, String docPath,
+      Map<String, dynamic> dataNeedUpdate) {
+    return firebaseFirestore
+        .collection(collectionPath)
+        .doc(docPath)
+        .update(dataNeedUpdate);
+  }
+
+  Stream<QuerySnapshot> getChatStream(String groupChatId, int limit) {
+    return firebaseFirestore
+        .collection(FirestoreConstants.pathMessageCollection)
+        .doc(groupChatId)
+        .collection(groupChatId)
+        .orderBy(FirestoreConstants.timestamp, descending: true)
+        .limit(limit)
+        .snapshots();
+  }
+
+  void sendMessage(String currentUserId, String groupChatId, String content) {
+    print("$currentUserId, $groupChatId, $content");
+    DocumentReference documentReference = firebaseFirestore
+        .collection(FirestoreConstants.pathMessageCollection)
+        .doc(groupChatId)
+        .collection(groupChatId)
+        .doc(DateTime.now().millisecondsSinceEpoch.toString());
+
+    MessageChat messageChat = MessageChat(
+      idFrom: currentUserId,
+      timestamp: DateTime.now().millisecondsSinceEpoch.toString(),
+      content: content,
+      sender: 1,
+    );
+
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      transaction.set(
+        documentReference,
+        messageChat.toJson(),
+      );
+    });
+  }
+}
