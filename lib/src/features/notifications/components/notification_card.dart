d1f9a8370d4837d63562f8ca61a381f07519cf79
danielsleibi <sleibi48@gmail.com>
2023-05-21 13:42:08 +0300

    Created notification frontend and frontend, backend handler and registeration with FCM

---
+++ lib/src/features/notifications/components/notification_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class NotificationCard extends StatefulWidget  {
+  String title = 'Initial';
+  String message = 'Initial';
+  NotificationCard({super.key, required this.title, required this.message});
+
+  @override
+  NotificationCardState createState() => NotificationCardState();
+
+}
+
+class NotificationCardState extends State<NotificationCard>{
+  late String _title;
+  late String _message;
+  @override
+  void initState() {
+    _title = 'Unknown Title';
+    _message = 'Unknown Message';
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: SizedBox(
+          width: null,
+          height: 125,
+
+          child: Column(children: [Center(child: Text(widget.title,
+            style: const TextStyle(
+            color: Colors.blue, // Text color
+            fontSize: 24, // Font size
+            fontWeight: FontWeight.bold, // Font weight
+            fontStyle: FontStyle.normal, // Font style
+            letterSpacing: 2, // Spacing between letters
+            wordSpacing: 4, // Spacing between words
+          ),)),
+            Center(child: Text(widget.message))]),
+        ),
+      ),
+    );
+  }
+
+}
